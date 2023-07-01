5bb886f7a2faa77e229077fd5e9234f32d237123
root <mtrhoades0727@gmail.com>
2023-06-30 23:27:58 -1000

    initial setup

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactGA from "react-ga";
+
 
 function App() {
+  ReactGA.initialize("UA-000000-01");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Nav Bar first
+    </>
   );
 }
 
