63686da563a519221b7c2d86578f89457d0e2230
root <mtrhoades0727@gmail.com>
2023-07-07 14:32:51 -1000

    started contact component - needs styling

--- front-end/src/components/Contact.tsx
+++ front-end/src/components/Contact.tsx
@@ -1,8 +1,39 @@
-
+import { MdOutlinePhoneIphone } from 'react-icons/md';
+import { MdOutlineEmail } from 'react-icons/md';
+import { MdOutlineLocationOn } from 'react-icons/md';
+import { LuFacebook } from 'react-icons/lu';
+import { FiInstagram } from 'react-icons/fi';
+import { FiTwitter } from 'react-icons/fi';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="contact contactContainer">
+        <h2>Contact Us</h2>
+        <div className="contact contactItems">
+            <div className="contact firstContactItems">
+                <div className="contact phone">
+                    <MdOutlinePhoneIphone /> (808) 442-3063
+                </div>
+                <div className="contact email">
+                    <MdOutlineEmail /> sales@mauilandbroker.com
+                </div>
+                <div className="contact location">
+                    <MdOutlineLocationOn /> 104 N. Market Street Wailuku, HI 96793
+                </div>
+            </div>
+            <div className="contact secondContactItems">
+                <div className="contact facebook">
+                    <LuFacebook /> 
+                </div>
+                <div className="contact instagram">
+                    <FiInstagram />
+                </div>
+                <div className="contact twitter">
+                    <FiTwitter />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
