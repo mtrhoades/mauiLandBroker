fa9151ba2bc1e685c93dfdc16937bd2b66347c50
root <mtrhoades0727@gmail.com>
2023-08-25 11:57:40 -1000

    reformatted home page look, put search bar ontop of blown up carousel images, better looking UI

--- front-end/src/layouts/Footer.tsx
+++ front-end/src/layouts/Footer.tsx
@@ -22,15 +22,15 @@ const NewFooter = () => {
 
   return (
     <div className='footer'>
-        <Container className="overflow-hidden">
-        <Row className='text-center text-md-start gx-5' style={{display: 'flex', justifyContent: 'center'}}>
-            <Col lg={4} md={6} sm={12} className='mb-3'>
+        <Container fluid>
+        <Row className='text-center text-md-start' style={{display: 'flex', justifyContent: 'center'}}>
+            <Col lg={3} md={6} sm={12} className='mb-3'>
                 <ListGroup>
                     <ListGroup.Item className='logo'><h4>Maui Land Broker & Property Management</h4></ListGroup.Item>
                     <ListGroup.Item>We here at MLB strive to keep the culture of Maui alive and well. Our roots run deep here and we want nothing more than to preserve the communities of Maui that have been here since the beginning.</ListGroup.Item>
                 </ListGroup>
             </Col>
-            <Col lg={2} md={6} sm={12} className='mb-3'>
+            <Col lg={1} md={6} sm={12} className='mb-3'>
                 <ListGroup>
                     <ListGroup.Item className='footer-link-heading'><h5>Other Links</h5></ListGroup.Item>
                     <ListGroup.Item> <a href='/specials'>Kama'āina Specials</a></ListGroup.Item>
@@ -38,10 +38,10 @@ const NewFooter = () => {
                     <ListGroup.Item> <a href='#'>Something Else</a></ListGroup.Item>
                 </ListGroup>
             </Col>
-            <Col lg={3} md={6} sm={12} className='mb-3 px-4 iconsCol'>
-                <div style={{paddingBottom: '20px'}} className='d-flex'>
+            <Col lg={2} md={6} sm={12} className='mb-3 iconsCol'>
+                <div>
                     <a href='https://www.facebook.com/MauiLandBroker/' target="_blank">
-                        <div className='social-icon' style={{paddingRight: '60px'}}>
+                        <div className='social-icon' style={{paddingRight: '60px', paddingBottom: '8px'}}>
                             <SiFacebook/>
                         </div>
                     </a>
@@ -50,57 +50,54 @@ const NewFooter = () => {
                             <FiInstagram/>
                         </div>
                     </a>
-                    {/* <a href='#'>
-                        <div className='social-icon'>
-                            <FiTwitter/>
-                        </div>
-                    </a> */}
                 </div>
-                <div style={{paddingBottom: '20px'}} className="phoneSection">
-                    <a href='#'>
-                        <div className='social-icon phoneIcon'>
-                            <MdOutlinePhoneIphone/>
-                        </div>
-                    </a>
-                    <h4>(808)442-3036</h4>
-                </div>
-                <div className="email">
-                    <button style={{backgroundColor: '#3B2313'}} onClick={handleShow}>
-                        <div style={{paddingRight: '10px'}} className="social-icon">
-                            <MdOutlineEmail />
-                        </div>
-                    </button>
-                    <h4>Message Us!</h4>
-                    <div>
-                        <Modal show={show} onHide={handleClose}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>sales@mauilandbroker.com</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <form className="contactForm">
-                            <label htmlFor="name">Name:</label>
-                            <input type="text" id="name" name="name"></input>
+                <div>
+                    <div style={{paddingBottom: '10px'}} className="phoneSection">
+                        <a href='#'>
+                            <div className='social-icon phoneIcon'>
+                                <MdOutlinePhoneIphone/>
+                            </div>
+                        </a>
+                        <h4>(808)442-3036</h4>
+                    </div>
+                    <div className="email">
+                        <button style={{backgroundColor: '#3B2313'}} onClick={handleShow}>
+                            <div style={{paddingRight: '10px'}} className="social-icon">
+                                <MdOutlineEmail />
+                            </div>
+                        </button>
+                        <h4>Message Us!</h4>
+                        <div>
+                            <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>sales@mauilandbroker.com</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <form className="contactForm">
+                                <label htmlFor="name">Name:</label>
+                                <input type="text" id="name" name="name"></input>
 
-                            <label htmlFor="subject">Subject:</label>
-                            <input type="text" id="subject" name="subject"></input>
+                                <label htmlFor="subject">Subject:</label>
+                                <input type="text" id="subject" name="subject"></input>
 
-                            <label htmlFor="message">Message:</label>
-                            <textarea id="message" name="message"/>
-                            </form>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="primary" onClick={handleClose}>
-                            Send Message
-                            </Button>
-                        </Modal.Footer>
-                        </Modal>
+                                <label htmlFor="message">Message:</label>
+                                <textarea id="message" name="message"/>
+                                </form>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="primary" onClick={handleClose}>
+                                Send Message
+                                </Button>
+                            </Modal.Footer>
+                            </Modal>
+                        </div>
                     </div>
                 </div>
             </Col>
             <Col lg={3} md={6} sm={12} className='mb-3'>
                 <div className="location">
                     <h4><MdOutlineLocationOn /> 104 N. Market St. Wailuku, HI 96793</h4>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3727.5656252104063!2d-156.50498312480417!3d20.889551380725173!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7954d36a70467cfb%3A0xf60fb39b11c5c07a!2sMaui%20Land%20Broker%20And%20Property%20Management%20Inc!5e0!3m2!1sen!2sus!4v1689807358563!5m2!1sen!2sus" width="350" height="250" loading="lazy"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3727.5656252104063!2d-156.50498312480417!3d20.889551380725173!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7954d36a70467cfb%3A0xf60fb39b11c5c07a!2sMaui%20Land%20Broker%20And%20Property%20Management%20Inc!5e0!3m2!1sen!2sus!4v1689807358563!5m2!1sen!2sus" width="425" height="300" loading="lazy"></iframe>
                 </div>
             </Col>
         </Row>
